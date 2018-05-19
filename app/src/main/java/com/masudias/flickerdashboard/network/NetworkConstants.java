f7bd0d9bb6f5f41ce41784c3711c13af21032d37
Reaz Murshed <reaz.murshed.masud@gmail.com>
2018-05-19 23:02:31 +0600

    Moved flicker per page default value hard coded constant to a final variable

--- app/src/main/java/com/masudias/flickerdashboard/network/NetworkConstants.java
+++ app/src/main/java/com/masudias/flickerdashboard/network/NetworkConstants.java
@@ -6,13 +6,14 @@ public class NetworkConstants {
     private static final String FLICKR_API_KEY = "949e98778755d1982f537d56236bbb42";
     private static final String FLICKR_BASE_URL = "https://api.flickr.com/services/rest/";
     private static final String FLICKR_SEARCH_METHOD = "flickr.photos.search";
+    private static final String FLICKR_PER_PAGE_DEFAULT = "10";
 
     public static String getUrlForFlickrImage(String tag, int page) {
         Uri.Builder uri = Uri.parse(FLICKR_BASE_URL).buildUpon();
         uri.appendQueryParameter("method", FLICKR_SEARCH_METHOD);
         uri.appendQueryParameter("api_key", FLICKR_API_KEY);
         uri.appendQueryParameter("page", page + "");
-        uri.appendQueryParameter("per_page", 5 + "");
+        uri.appendQueryParameter("per_page", FLICKR_PER_PAGE_DEFAULT);
         uri.appendQueryParameter("format", "json");
         uri.appendQueryParameter("nojsoncallback", "1");
         uri.appendQueryParameter("tags", tag);
